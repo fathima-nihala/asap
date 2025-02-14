b03e58fabe5ae1ce43f9a529b5991cf53bc52ff8
Fathima Nihala <137775396+fathima-nihala@users.noreply.github.com>
2025-02-14 12:33:54 +0530

    Update index.js

--- asap_server/index.js
+++ asap_server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://asap-6.onrender.com'],
+    origin: ['https://asap-6.onrender.com', 'http://localhost:3000'],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
