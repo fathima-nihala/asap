f0113c3424f14798b218e6ac5c8038ad9343acff
Fathima Nihala <137775396+fathima-nihala@users.noreply.github.com>
2025-02-14 15:55:10 +0530

    Update index.js

--- asap_server/index.js
+++ asap_server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
 app.use(cors({
-    origin: ['https://asap-6.onrender.com/', 'http://localhost:3000', 'https://asap-frontend.onrender.com/'],
+    origin: ['https://asap-6.onrender.com/', 'http://localhost:3000', 'https://asap-frontend.onrender.com/','https://asap-8tcj-lyztynfr4-fathima-nihalas-projects.vercel.app/'],
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
